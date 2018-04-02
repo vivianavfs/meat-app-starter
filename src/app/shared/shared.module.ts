d859739d4c9a0dd1e6bf1f571dedb7e6b36e37f3
vivianavfs <vivianavfs@yahoo.com>
2018-04-02 09:39:55 -0300

    module with providers

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,14 @@
-import { NgModule } from '@angular/core'
+import { NgModule, ModuleWithProviders } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { ReactiveFormsModule, FormsModule } from '@angular/forms'
 
 import { InputComponent } from './input/input.component'
 import { RadioComponent } from './radio/radio.component'
 import { RatingComponent } from './rating/rating.component'
+
+import {OrdemService} from '../order/order.service'
+import {RestaurantsService} from '../restaurants/restaurants.service'
+import {ShoppingCartService} from '../restaurant-detail/cart/shopping-cart.service'
 
 
 @NgModule({
@@ -14,4 +18,11 @@ import { RatingComponent } from './rating/rating.component'
 			CommonModule, ReactiveFormsModule, FormsModule]
 })
 
-export class SharedModule {}
+export class SharedModule {
+	static forRoot(): ModuleWithProviders {
+		return {
+			ngModule: SharedModule,
+			providers: [OrdemService,RestaurantsService,ShoppingCartService]
+		}
+	}
+}
